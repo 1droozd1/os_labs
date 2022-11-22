557ce3935eb95c39cf052256d00721edcef31184
1droozd1 <dubr0vin-dk@mail.ru>
2022-11-22 12:13:51 +0300

    first step

--- lab_4/src/child_process.c
+++ lab_4/src/child_process.c
@@ -2,20 +2,72 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int sum_from_char(char *s)
+{
+    int i = 0, sum = 0, n = 0, flag1 = 0;
+    int len = strlen(s);
+
+    for (int i = 0; i < len; ++i) {
+        if (s[i] >= '0' && s[i] <= '9' && flag1 == 0) {
+            n = n * 10 + (s[i] - '0');
+        } else if (flag1 == 1 && s[i] >= '0' && s[i] <= '9') {
+            n = n * 10 - (s[i] - '0');
+        } else if (s[i] == '-') {
+            flag1 = 1;
+        } else if (s[i] == ' ' && flag1 == 1) {
+            flag1 = 0;
+            sum += n;
+            n = 0;
+            continue;
+        } else if (n) {
+            sum += n;
+            n = 0;
+            flag1 = 0;
+        }
+    }
+    return sum + n;
+}
 
 int main(int argc, char const *argv[])
 {
-    //*pointer = argv[0];
-    char read_file_name[20];
+    char read_file_name[] = argv[0];
+    int str_len  = atoi(argv[1]);
 
+    printf("[Child Process,  id=%d]", getpid());
 
-    /*remove(read_file_name); //if we have the same file
-    FILE *write_res;
-    if ((write_res = fopen(read_file_name, "w")) == NULL) {
-        printf("Error: can't open file\n");
-        exit(1);
-    }*/
+    int memFd = shm_open(read_file_name, O_RDONLY, 0);
+    if (memFd == -1) {
+        perror("Can't open file");
+        return 1;
+    }
+    void *buffer = mmap(NULL, str_len, PROT_READ, MAP_SHARED, memFd, 0);
+    if (buffer == NULL) {
+        perror("Can't mmap");
+        return -1;
+    }
 
+    char *str_ptr = malloc(str_len);
+    int i = 0;
+
+    fscanf(read_file_name, "%s", str_ptr);
+
+    printf("%s\n", str_ptr); 
+
+    int res = sum_from_char(str_ptr);
+    free(str_ptr);
+
+    fprintf(read_file_name, "%d", res);
+
+    //??
+    if (munmap(buffer, str_len) == -1) {
+        perror("munmao");
+        return 1;
+    }
 
     return 0;
 }
--- lab_4/src/main.c
+++ lab_4/src/main.c
@@ -1,22 +1,44 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "unistd.h"
-#include "sys/mman.h"
-#include "fcntl.h"
-#include "semaphore.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <semaphore.h>
 
-#include "functions.h"
+typedef struct numbers
+{
+    int num;
+    int st;
+    char *file_name;
+} numbers;
 
+int human_get(sem_t *semaphore)
+{
+    int s;
+    sem_getvalue(semaphore, &s);
+    return s; 
+}
 
+void human_set(sem_t *semaphore, int n)
+{
+    while (human_get(semaphore) < n)
+    {
+        sem_post(semaphore);
+    }
+    while (human_get(semaphore) > n)
+    {
+        sem_wait(semaphore);
+    }
+}
 
 
 int main(int args, char *argv[])
 {
     pid_t id;
-    int sum = 0;
+    int sum = 0, n = 0;
     numbers* mapped = (numbers*)mmap(0, sizeof(numbers), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0, 0);
     char file_name[20];
-    char sequence_of_numbers[100];
 
     if (mapped == MAP_FAILED)
     {
@@ -25,6 +47,8 @@ int main(int args, char *argv[])
     }
 
     id = fork();
+    sem_unlink("_sem");
+    sem_t *sem = sem_open("_sem", O_CREAT, 0, 2);
 
     if (id < 0) {
         perror("fork error");
@@ -33,21 +57,87 @@ int main(int args, char *argv[])
     // Child Process
     else if (id == 0) {
 
-        execlp ("./child_process", mapped, NULL);
+        /*execlp ("./child_process", mapped, NULL);
         fprintf(stderr, "\nExec didn't work...\n");
-        exit(1);
+        exit(1);*/
+        char file_name_read[20] = "123";
+
+        remove(file_name_read);
+
+        FILE *write_res;
+        if ((write_res = fopen(file_name_read, "w")) == NULL) {
+            printf("Error: can't open file\n");
+            exit(1);
+        }
+
+        while(1) 
+        {
+            while(human_get(sem) == 2)
+            {
+                continue;
+            }
+            if (mapped->st == 1) 
+            {
+                sum += mapped->num;
+                fprintf(write_res, "%d", sum);
+                sum = 0;
+                human_set(sem, 2);
+            }
+            else if (mapped->st == 2)
+            {
+                sum += mapped->num;
+                fprintf(write_res, "%d", sum);
+                fclose(write_res);
+                human_set(sem, 0);
+                exit(0);
+            }
+            else if (mapped->st == 0)
+            {
+                sum += mapped->num;
+                human_set(sem, 2);
+            }
+        }
 
     }
     //Parent process
     else {
         printf("[Parent Process, id=%d]: Write name of file: ", getpid());
-        fgets(file_name, 20, stdin);
+        /*fgets(file_name, 20, stdin);
         if (file_name[strlen(file_name) - 1] == '\n')
             file_name[strlen(file_name) - 1] = '\0';
+
+        mapped->file_name = (char *)file_name;*/
+
+        while(human_get(sem) != 0) 
+        {
+            char c;
+            scanf("%d%c", &n, &c);
+            mapped->num = n;
+            if (c == ' ')
+            {
+                mapped->st = 0;
+            }
+            if (c == '\n')
+            {
+                mapped->st = 1;
+            }
+            if (c == '\0')
+            {
+                mapped->st = 2;
+            }
+            human_set(sem, 1);
+            while(human_get(sem) == 1)
+            {
+                continue;
+            }
+        }
 
     }
 
 
     munmap(mapped, sizeof(numbers));
+    sem_close(sem);
+    sem_destroy(sem);
+
     return 0;
 }
